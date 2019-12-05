284295d00863240ae3d40ee570a74c21cf7cd459
chrismath <c.ntinopoulos@gmail.com>
2019-12-05 20:14:24 +0200

    Working get User service add missing files

--- src/main/java/com/skyroof/MyFirstSpringBoot/MyFirstSpringBootApplication.java
+++ src/main/java/com/skyroof/MyFirstSpringBoot/MyFirstSpringBootApplication.java
@@ -2,11 +2,15 @@ package com.skyroof.MyFirstSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication //dilwnei oti einai i arxiki me8odos toy spring boot
 @ComponentScan("com.skyroof") //psaxnei olo to project kai briskei annotations ta opoia
 //enswmatwnei mesa sto project
+@EntityScan("com.skyroof")
+@EnableJpaRepositories("com.skyroof")
 public class MyFirstSpringBootApplication {
 
 	public static void main(String[] args) {
