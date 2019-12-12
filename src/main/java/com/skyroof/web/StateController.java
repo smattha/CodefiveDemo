08702855f3aa4e01e90e823ec27fd855b39ac19b
Matthaiakis Stergios <stergiosmat@gmail.com>
2019-12-12 17:33:00 +0200

    --Smatt modification to the user controller function

--- src/main/java/com/skyroof/web/StateController.java
+++ src/main/java/com/skyroof/web/StateController.java
@@ -1,11 +1,11 @@
 package com.skyroof.web;
 
+import com.skyroof.skyroof.SkyroofServer;
 import com.skyroof.dao.*;
 import com.skyroof.model.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -19,8 +19,11 @@ public class StateController {
 
     @ResponseBody
     @GetMapping("/getStatus")
-    public List<IssuestatusEntity> getAllUsers() {
+    public List<IssuestatusEntity> getStates() {
+        SkyroofServer.logger.info("Skyroof microservice get states was called");
         List<IssuestatusEntity> all = (List<IssuestatusEntity>) statusDao.findAll();
+        SkyroofServer.logger.info(all.toString());
+        SkyroofServer.logger.info("Skyroof  microservice is about to return");
         return all;
     }
 
